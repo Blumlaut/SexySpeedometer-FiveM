18c8cb62af193ce9810a39411dc9fe3f969d4267
Blue <13604413+Blumlaut@users.noreply.github.com>
2021-02-03 01:46:27 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -3,13 +3,8 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 client_scripts {
 	"client.lua",
 
-	-- add skins here
-	"skins/default.lua",
-	"skins/default_middle.lua",
-	"skins/id4.lua",
-	"skins/id5.lua",
-	"skins/id6.lua",
-	"skins/id7.lua",
+	-- DONT ADD SKINS HERE, *.lua MEANS ALL .LUA FILES!!!
+	"skins/*.lua",
 
 
 }
